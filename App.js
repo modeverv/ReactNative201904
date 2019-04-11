8b2d41f8f65849170ae4b1907e3d89556be3151b
modeverv <modeverv@gmail.com>
2019-04-11 20:27:11 +0900

    2019-04-11 20:27:11

--- App.js
+++ App.js
@@ -9,58 +9,38 @@
 import React, {Component} from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
 type Props = {};
 export default class App extends Component<Props> {
-  state = {
-    message: 'Message'
-  };
   render() {
     return (
       <View style={styles.container}>
-        <HelloWorld message={this.state.message} />
-        <Text
-          style={styles.welcome}
-          onPress={ () => this.setState({message: 'onPress'}) }
-        >
-          ハローワールド{this.state.message}
-        </Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+        <View style={styles.box3} />
       </View>
     );
-  }
-}
-
-class HelloWorld extends Component {
-  render() {
-    return (<Text style={styles.welcome}>Hellllll {this.props.message}</Text>)
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    //    flexDirection: 'column',
+    flexDirection: "row",
   },
-  welcome: {
-    color: '#AAA',
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 20,
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: "yellow"
   },
+  box3: {
+    width: 100,
+    height: 100,
+    backgroundColor: "blue"
+  }
 });
